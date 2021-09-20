d59fc09bd21346e095285eb8ce9b7a2da0ef99bb
chiragJKL <chirag.lunagaria4@gmail.com>
2021-09-20 05:49:24 -0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        //Scaffold = is like a wraper of all widgets given below
+//Scaffold = is like a wraper of all widgets given below
         appBar: AppBar(
           title: Text('PRANAM'),
           centerTitle: true,
+          backgroundColor: Colors.red[600],
         ),
         body: Center(
-          child: Text('Welcome! This is my first app'),
+          child: Text(
+            'Welcome! This is my first app',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.2,
+              color: Colors.black,
+//changes made in "pubspec.yaml" and fonts added from google font
+              fontFamily: 'Carattere',
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text('click'),
+          backgroundColor: Colors.red[600],
         ),
       ),
     ));
